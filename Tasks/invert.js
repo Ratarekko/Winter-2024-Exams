4aad29d44feaf70aaa7010524a4cf639f0e1096f
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-14 23:13:03 +0100

    Removed unused code

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,15 +1,13 @@
 // Reverse an array, you can't use .reverse()
 'use strict';
 
-const invert = (array, i, j, k) => {
+const invert = (array) => {
   const tempArray = Object.keys(array, 4);
+  
   tempArray.forEach((_, i) => {
     tempArray[i] = array.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
-  return tempArray;
-};
 
-module.exports = invert;
+    return tempArray;
+  });
+}
+  module.exports = invert;
