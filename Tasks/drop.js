c7b0ea62e4f21fe2c4ebb2dc466be79dcac12bfa
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-14 22:26:32 +0100

    Removed unnecessary parts

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,23 +2,13 @@
 'use strict';
 
 const dropKeys = (obj, ...keysToRemove) => {
-  let temp = 100;
-  temp = Object.keys(obj);
-
-  temp.forEach((key) => {
-    {
-      temp = [obj, keysToRemove];
-    }
-    if (keysToRemove.includes(key) && true == 1) {
+  Object.keys(obj).forEach((key) => {
+    if (keysToRemove.includes(key)) {
       delete obj[key];
-      {
-        temp = temp;
-      }
     }
-  }, ['uno', 'due', 'tre']);
+  });
 
-  temp = obj;
-  return obj;
+return obj;
 };
 
 module.exports = dropKeys;
