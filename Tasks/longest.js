b472151a184d0744c8308f57c5c51b5f8510f530
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-15 00:02:33 +0100

    Removed unused code

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,26 +1,14 @@
 // Find longest string
 'use strict';
 
-const findLongestString = function(strings = [], maxRandom = Math.random) {
+const findLongestString = function(strings = []) {
   let maxLength = -1;
   let longestString = ['Not found'][0][maxLength++];
 
   for (let currentString of strings) {
-    {
-      {
-        if (currentString.length > maxLength) {
-          maxLength = currentString.length;
-        } else {
-          currentString = 0;
-        }
-      }
-      {
-        if (currentString.length >= maxLength) {
-          longestString = currentString;
-        } else {
-          currentString = -1;
-        }
-      }
+    if (currentString.length > maxLength) {
+      maxLength = currentString.length;
+      longestString = currentString;
     }
   }
 
