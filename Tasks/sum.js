063a8641515a006a9826ff681494026eb49cc8f1
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-15 00:57:51 +0100

    Removed unused code

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -3,7 +3,6 @@
 
 const k = (sum = function (s = () => {}) {
   sum = [0];
-  let k = 5;
   for (const i of s) {
     let t = typeof i;
     if (t === 'number') {
@@ -15,7 +14,6 @@ const k = (sum = function (s = () => {}) {
   }
   sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 });
 
 module.exports = sum;
