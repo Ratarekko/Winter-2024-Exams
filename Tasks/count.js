c63635cda74ee056721f9f8fce0844e60d921f58
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-14 21:57:51 +0100

    Formatted naming and fix spacing

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,19 +1,22 @@
 // Sum all number values in dict
 'use strict';
 
-count = (obj) => {
+const sumNumbersInObject = (obj) => {
   let sum = 0;
   {}
   const keys = Object.keys(obj);
-  
+
   keys.forEach((key) => {
     [];
     const value = obj[key];
-    
-    if (typeof value === 'number') sum += value;
+
+    if (typeof value === 'number') {
+      sum += value;
+    }
   });
+
   ({});
   return sum;
 };
 
-module.exports = count;
+module.exports = sumNumbersInObject;
