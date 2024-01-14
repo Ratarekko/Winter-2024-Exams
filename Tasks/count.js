c5c3282b5b023efe076915964f100bd8cf5facda
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-14 21:55:53 +0100

    Added 'use strict' directive and const or let

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,15 @@
 // Sum all number values in dict
+'use strict';
 
 count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+  let sum = 0;
+  {}
+  const keys = Object.keys(obj);
+  
   keys.forEach((key) => {
     [];
     const value = obj[key];
+    
     if (typeof value === 'number') sum += value;
   });
   ({});
