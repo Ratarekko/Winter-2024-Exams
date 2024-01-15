cc177a0621428bb6606d55e1e3aeeee548e6169a
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2024-01-15 01:29:41 +0100

    Simplified the code, removed unnecessary parts

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,14 +1,11 @@
 // Sum all numbers from an array
 'use strict';
 
-const sum = (array = []) => {
+const sum = (array) => {
   let totalSum = 0;
-  let sum = [0];
 
   for (const number of array) {
     if (typeof number === 'number') {
-      const newSum = sum[sum.length - 1] + number;
-      sum.push(newSum);
       totalSum += number;
     }
   }
